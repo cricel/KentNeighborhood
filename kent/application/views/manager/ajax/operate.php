ea7d45464b62aba2f5e7a5e8c0a20a338124e0a8
cricel <cricel.design@gmail.com>
2018-03-06 01:17:19 -0500

    update

--- kent/application/views/manager/ajax/operate.php
+++
@@ -1,7 +0,0 @@
-<?php
-$result = array(
-	'msg' => '操作成功！',
-	'result' => '0',
-	'code' => '101'
-);
-echo json_encode($result);
--- kent/application/views/tests/page/php/test1.php
+++
@@ -1,4 +0,0 @@
-<?php
-$data =  $_POST['data'];
-print_r($data);
-echo $data[0]['answer'];
